d612c276e009cb896060679096d23f5593fc63bc
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2026-09-02 17:58:40 -0300

    refactor

--- services/http_client.py
+++ services/http_client.py
@@ -6,6 +6,7 @@ import pandas as pd
 import re
 import datetime
 
+
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
